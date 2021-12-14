213399e377124c454e3dd037d2ecd1032decb3b9
Naveen Adigopula <Naveen.Adigopula@BRILLIO.COM>
2021-12-14 21:29:27 +0530

    sop popups creation

--- src/components/sopModels/CreateSop.js
+++ src/components/sopModels/CreateSop.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import "./SopModels.scss";
 
 export default function CreateSop(props) {
     return (
         <div>
-            <Modal show={props.show}>
+            <Modal show={props.show} dialogClassName="createSop">
                 <Modal.Header onClick={props.onHide} closeButton></Modal.Header>
                 <Modal.Body>
                     <div className="create_sop">
-                        <div className="create_sop_div">
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <input type="text" />
-                        </div>
+                        <h3>Create Sop</h3>
+                        <form>
+                            <fieldset>
+                                <div className="field buildSop">
+                                    <label>Build Sop</label>
+                                    <input type="text" placeholder="Build Sop" />
+                                </div>
+                                <div className="field uploadFile">
+                                    <label>Upload For Desktop</label>
+                                    <input
+                                        type="file"
+                                        placeholder="Upload For Desktop"
+                                    />
+                                </div>
+                            </fieldset>
+                        </form>
                     </div>
                 </Modal.Body>
 
                 <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
+                    <Button onClick={props.onHide}>Cancel</Button>
                 </Modal.Footer>
             </Modal>
         </div>
--- src/components/sopModels/CreateSoptype.js
+++ src/components/sopModels/CreateSoptype.js
@@ -4,21 +4,28 @@ import { Button, Modal } from "react-bootstrap";
 export default function Create_Sop_type(props) {
     return (
         <div>
-            <Modal show={props.show}>
+            <Modal show={props.show} dialogClassName="createSopType">
                 <Modal.Header onClick={props.onHide} closeButton></Modal.Header>
                 <Modal.Body>
                     <h3>Create SOP Type</h3>
-                    <div>
-                        <label>SOP Type</label>
-                        <input type="text" placeholder="Adminstrative SOP" />
-                    </div>
-                    <div>
-                        <label>Description</label>
-                        <input type="text" />
-                    </div>
-                    <div>
-                        <Button>Create</Button>
-                    </div>
+                    <form>
+                        <fieldset>
+                            <div className="field">
+                                <label>SOP Type</label>
+                                <input
+                                    type="text"
+                                    placeholder="Adminstrative SOP"
+                                />
+                            </div>
+                            <div className="field">
+                                <label>Description</label>
+                                <input type="text" placeholder="Description" />
+                            </div>
+                        </fieldset>
+                        <div className="actions">
+                            <Button>Create</Button>
+                        </div>
+                    </form>
                 </Modal.Body>
             </Modal>
         </div>
